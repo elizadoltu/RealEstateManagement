140594028526063d24e06d558d5ea4f306f772f5
Cezara <aprofirei.cezara@gmail.com>
2024-12-02 22:17:45 +0200

    Modified User CreateCommand

--- Application/Use Cases/Users/Commands/CreateUserCommand.cs
+++ Application/Use Cases/Users/Commands/CreateUserCommand.cs
@@ -9,7 +9,5 @@ namespace Application.Use_Cases.Commands
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public List<PropertyListing>? propertyListings { get; set; }
-        public List<ClientInquiry>? clientInquiries { get; set; }
     }
 }
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,8 +6,6 @@
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
-        //public List<PropertyListing>? propertyListings { get; set; }
-        //public List<ClientInquiry>? clientInquiries { get; set; }
         
     }
 }
