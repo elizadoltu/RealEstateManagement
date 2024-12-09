7b9ee69e9111c5546881190e21661f841f36c8af
melintech <cosminmelinte24@gmail.com>
2024-12-09 19:37:16 +0200

    added authentication

---
+++ Application/Use Cases/Authentication/LoginUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using MediatR;
+
+public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, string>
+{
+    private readonly IUserAuthRepository userRepository;
+
+    public LoginUserCommandHandler(IUserAuthRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public async Task<string> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = new User
+        {
+            Email = request.Email,
+            PasswordHash = request.Password
+        };
+        var token = await userRepository.Login(user);
+        return token;
+    }
+}
--- RealEstateManagement/Program.cs
+++ RealEstateManagement/Program.cs
@@ -1,5 +1,7 @@
 using Application;
+using Identity;
 using Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,12 +19,43 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddIdentity(builder.Configuration);
+
 builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+    // Add JWT Authentication
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\""
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -40,6 +73,9 @@ app.UseRouting();
 app.UseCors("MyAllowSpecificOrigins");
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
