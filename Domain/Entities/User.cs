84ef99d92e64d276d20a3c8da7dab7395b00c3a7
Doltu Teodora - Eliza <elizadoltuofficial@gmail.com>
2024-12-15 20:54:52 +0200

    changed required password

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,6 +6,6 @@
         public string? Name { get; set; }
         public required string Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public required string PasswordHash { get; set; }
+        public string? PasswordHash { get; set; }
     }
 }
